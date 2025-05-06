f094d469a36e1e3a8abe3216c7fc5cbe3fd526cd
najmul <mdnajmulislam10992@gmail.com>
2025-05-06 21:59:33 +0600

    create navbar and footer basic structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+include('index/header.php');
+include('index/navbar.php');
+?>
+
+<div class="py-5">
+<div class="container">
+    <div class="row">
+
+    <div class="col-md-12">
+        <h4>Login And Registration System</h4>
+        <h5>With Email verification</h5>
+    </div>
+
+    </div>
+</div>
+
+</div>
+<?php include('index/footer.php') ?>
---
+++ index/navbar.php
@@ -0,0 +1,29 @@
+<div class="bg-dark">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+  <a class="navbar-brand" href="#">KhbarChuri</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ms-auto">
+     
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Regiter</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+  </div>
+</div>
+</div>
+
